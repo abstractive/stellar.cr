1fa75037faacca68c6e1013c4036fcad0c36a3fc
digitalextremist // <code@extremist.digital>
2019-12-18 09:22:55 -0800

    closes #25

--- face/src/components/drawers/Landing.vue
+++ face/src/components/drawers/Landing.vue
@@ -4,60 +4,47 @@
       elevated bordered
       :mini="!fullDrawer"
       show-if-above
-      @click.capture="clickDrawer"
       v-model="showDrawer"
       :breakpoint="500"
-      content-class="bg-grey-5">
+      content-class="bg-grey-4">
         <q-list
-          class="fit q-py-md"
           highlight
           link>
-          <q-item clickable class='q-mb-md' to="/about">
-            <q-item-section side avatar><q-icon name="fas fa-star-of-life" /></q-item-section>
-            <q-item-section>What is Stellar?</q-item-section>
-          </q-item>
-
+          <template v-for='option in options'>
+            <template v-if='option == "separator"'>
+              <q-separator v-bind:key='option.type' />
+            </template>
+            <template v-else>
+              <q-item v-bind:key='option.label'
+                clickable class='q-my-md'
+                v-bind='routeOption(option)'
+                v-on:click='clickOption(option)'>
+                <q-item-section side avatar>
+                  <q-icon class='text-grey-10' :name="option.icon" />
+                </q-item-section>
+                <q-item-section class='text-grey-8'>{{option.label}}</q-item-section>
+                <q-tooltip
+                  v-if='showDrawerTooltip'
+                  content-class="q-pa-sm q-px-lg  bg-deep-orange-6"
+                  content-style="font-size: 16px"
+                  anchor="center right"
+                  self="center left"
+                  :offset="[10, 10]">
+                  {{option.label}}
+                </q-tooltip>
+              </q-item>
+            </template>
+          </template>
           <q-separator />
-
-          <q-item clickable class='q-my-md' to="/contribute">
-            <q-item-section side avatar>
-              <q-icon name="fas fa-fist-raised" />
+          <q-item clickable
+            @click.capture='toggleSidebar'>
+            <q-item-section class='q-my-md' side avatar>
+              <q-icon class='text-grey-6'
+                :name='(!fullDrawer) ? "fas fa-chevron-right" : "fas fa-chevron-left"' />
             </q-item-section>
-            <q-item-section>Contribute</q-item-section>
+            <q-item-section class='text-grey-6'>Collapse Sidebar</q-item-section>
           </q-item>
-
-          <q-item clickable class='q-my-md' to="/handbook">
-            <q-item-section side avatar><q-icon name="fas fa-book" /></q-item-section>
-            <q-item-section>Handbook</q-item-section>
-          </q-item>
-
-          <q-item clickable class='q-my-md' to="/support">
-            <q-item-section side avatar><q-icon name="fas fa-headset" /></q-item-section>
-            <q-item-section>Get Support</q-item-section>
-          </q-item>
-
-          <q-separator />
-
-          <q-item clickable class='q-my-md' @click.native="revealSignIn">
-            <q-item-section side avatar>
-              <q-icon name="fas fa-sign-in-alt" />
-            </q-item-section>
-            <q-item-section>Sign In</q-item-section>
-          </q-item>
-
         </q-list>
-      <div
-        class="q-mini-drawer-hide absolute"
-        style="top: 15px; right: -17px">
-          <q-btn
-            dense
-            round
-            unelevated
-            color="accent"
-            icon="fas fa-chevron-left"
-            @click.capture="miniDrawer"
-          />
-        </div>
     </q-drawer>
   </div>
 </template>
@@ -67,9 +54,51 @@
 export default {
   data () {
     return {
+      options: [
+        {
+          icon: 'fas fa-star-of-life',
+          label: 'What is Stellar?',
+          to: '/about'
+        },
+        'separator',
+        {
+          icon: 'fas fa-fist-raised',
+          label: 'Contribute',
+          to: '/contribute'
+        },
+        {
+          icon: 'fas fa-book',
+          label: 'Handbook',
+          to: '/handbook'
+        },
+        {
+          icon: 'fas fa-headset',
+          label: 'Get Support',
+          to: '/support'
+        },
+        'separator',
+        {
+          icon: 'fas fa-sign-in-alt',
+          label: 'Sign In',
+          click: this.revealSignIn
+        }
+      ]
     }
   },
   methods: {
+    clickOption (o) {
+      if ('click' in o) {
+        console.log(o.click)
+        o.click()
+      }
+    },
+    routeOption (o) {
+      if ('to' in o) {
+        return {
+          to: o.to
+        }
+      }
+    },
     revealSignIn () {
       this.$store.dispatch('showSignIn')
     },
@@ -84,7 +113,8 @@ export default {
         this.$store.dispatch('fullDrawer')
       }
     },
-    toggleDrawer (e) {
+    toggleSidebar (e) {
+      (!this.fullDrawer) ? this.clickDrawer() : this.miniDrawer()
       e.stopPropagation()
     }
   },
@@ -106,6 +136,9 @@ export default {
     },
     showSignIn () {
       return this.$store.state.showSignIn
+    },
+    showDrawerTooltip () {
+      return !this.fullDrawer
     }
   }
 }
